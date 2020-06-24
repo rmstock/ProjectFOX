682080b7e8ae7643321ee159eb01a5e42dde6911
Charles Hasler <hasler.charles@gmail.com>
2020-06-24 11:35:09 -0600

    Created new images, got image/buttons "working"
    added new images (.png) so they look a little better
    going to use the alternate colors for clicking/gestures
    trash can button now deletes a row
    pinning just changes the pin color, future we'll have to change that to
    do whatever we want it to do

--- javascript/prototype.js
+++ javascript/prototype.js
@@ -1,9 +1,8 @@
-//This is the connection link to the backend. (Not working right now)
+// This is the connection link to the backend. (Not working right now)
 const JSONDATA = "/ProjectFOX/javascript/prototype.json";
 getData(JSONDATA);
 
-
-//send JsonData
+// Send JsonData
 function getData(data) {
     fetch(data)
         .then(response => response.json())
@@ -15,11 +14,10 @@ function getData(data) {
         .catch(error => console.log('There was an error: ', error))
 } // end getData function
 
-//Insert Table of Tracks Data. (Not working right now)
+// Insert Table of Tracks Data. (Not working right now)
 function makeTable(data) {
     // Log what is returned
     console.log(data);
-
     // Build table elements
     // Use a for loop to include the results in list items
     let list = "<tr>";
@@ -27,14 +25,14 @@ function makeTable(data) {
         list += "<td>" + data.Table[i] + "</td>";
         console.log('tracks data:');
         console.log(data.Table[i]);
-    };
+    }
     list += "</tr>";
     scrollbar.innerHTML = list;
 }
 
-
-//On the menu bar, when you click "All, or "high accuracy" etc, show only that data. Send page specific data to display function below. (Not working right now)
-
+// On the menu bar, when you click "All, "High accuracy" etc,
+// show only that data. Send page specific data to display function below.
+// (Not working right now)
 const navbar = document.getElementById("navigation");
 navbar.addEventListener("click", function (event) {
 
@@ -57,30 +55,32 @@ navbar.addEventListener("click", function (event) {
     }
 })
 
-
-//Take target data and put into webpage
-//my id is where JSON data gets inserted.
-//what goes in is the prototype.json data."menu" of section I want to insert.
+// Take target data and put into webpage
+// my id is where JSON data gets inserted.
+// what goes in is the prototype.json data."menu" of section I want to insert.
 function productSpecific(content, data) {
-
-    //check what it thinks is the target data (object)
+    // Check what it thinks is the target data (object)
     console.log(data[content].menu);
-
 }
 
-//This works. Right now it changes the color of the specific kill button clicked. Each tract will need a specific ID associated with it, so that only that tract is marked killed when the button is clicked. We will also add functionality here to send a message to the backend that this tract was killed so it can be sorted.
-function KILLActive(){
+// This works. Right now it changes the color of the specific kill button clicked. Each tract will need a specific ID associated with it, so that only that tract is marked killed when the button is clicked. We will also add functionality here to send a message to the backend that this tract was killed so it can be sorted.
+function KILLActive() {
     document.getElementById("KILL2").style.backgroundColor = "green";
     document.getElementById("KILL2").style.color = "black";
 }
 
-//This works. When the pilot clicks the fix button, it turns that tracks fix button yellow. We need to relay this to the backend still. Need the tracks ID to sync with Fix# ID so only that one track button changes color.
-function FIXActive(){
+// This works. When the pilot clicks the fix button, it turns that tracks fix button yellow. We need to relay this to the backend still. Need the tracks ID to sync with Fix# ID so only that one track button changes color.
+function FIXActive() {
     document.getElementById("FIX1").style.backgroundColor = "yellow";
     document.getElementById("FIX1").style.color = "black";
 }
 
-//This does not work yet. I was messing with some GUI gestures, but haven't figured it out yet.
-function SwipeLeftAction() {
-    document.getElementById("unique1").style.display = "none";
+// Click on trash can to delete rows
+function deleteRow() {
+    document.getElementById("main-table").deleteRow(0);
+}
+
+// Click on push pin to pin a row
+function pinRow(clickedID) {
+    document.getElementById(clickedID).classList.toggle("pinned");
 }
